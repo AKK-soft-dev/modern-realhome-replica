0ae5947532bbcf50f3b54ebe3d8aa6da76699ad9
AKK-soft-dev <aungkokosoftdev@gmail.com>
2023-11-11 12:34:09 +0630

    update: paddings

--- src/components/AmazingFeatures/AmazingFeatures.tsx
+++ src/components/AmazingFeatures/AmazingFeatures.tsx
@@ -7,14 +7,14 @@ const AmazingFeatures = () => {
     <>
       <Separator />
       <section className="bg-paper">
-        <div className="relative z-10 xl:custom-container pt-10 md:pt-0 pb-10">
+        <div className="relative z-10 xl:custom-container pt-10 md:pt-0 pb-10 px-4 sm:px-3">
           <h1 className="text-center text-primary-dark font-medium">Amazing</h1>
           <h2 className="text-center text-3xl">Features</h2>
           <p className="text-center text-black/60 mt-2 mb-7">
             Test featured from elementor.
           </p>
           <div
-            className={`grid grid-cols-1 md:grid-cols-3 gap-7 duration-200 `}
+            className={`grid grid-cols-1 md:grid-cols-3 gap-12 md:gap-5 duration-200 `}
           >
             {amazingFeatures.map((property) => (
               <AmazingFeatureItem key={property.id} data={property} />
